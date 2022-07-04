a25e4273f52bf7025ee6ad6764b3dc5aac1713f2
leovantine <leonardovantine2@gmail.com>
2022-07-03 21:01:59 -0300

    Basic structure and components

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
+
+export default function App(){
+  return(
+    <>
+    <GlobalStyle />
+    <h1>HOMEHEADER.js</h1>
+    </>
+  )
+}
