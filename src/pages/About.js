dffa056477f4c2eab10b9adb385cecf617fdf9f9
leovantine <leonardovantine2@gmail.com>
2022-07-21 01:31:47 -0300

    About page almost ready

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,50 +1,93 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import img1 from "../images/img1.jpg"
+import img2 from "../images/img2.jpg"
 
 import styled from 'styled-components'
 
 const Container = styled.div`
-border: solid red;
+/* border: solid red; */
 width: 100%;
-height: 87vh;
+height: 114vh;
 background-color: #1B1D22;
 `
 const FirstInfo = styled.div`
-border: solid pink;
+/* border: solid pink; */
 display: flex;
 justify-content: center;
 align-items: center;
-flex-direction: row;
 `
 const BorderImgDiv = styled.div`
 border: solid 6px #0263F5;
 border-radius: 5px;
-width: 12vw;
+width: 13.5vw;
 height: 42vh;
 margin: 2.5vw;
 `
 const Img = styled.img`
 width: 100%;
-height: 40.7vh;
+height: 40vh;
 margin: 2vh;
 border: solid 4px #CCCCCD;
 border-radius: 5px;
 `
 const P = styled.p`
 font-family: 'Ranade', sans-serif;
-font-size: 1.2rem;
+font-size: 1rem;
 font-weight: 600;
 color: #CCCCCD;
 width: 45vw;
-text-align: center;
+text-align: justify;
 `
-const LanguageButton = styled.button`
-width: 8vw;
-height: 5vh;
-margin-top: 47vh;
+const TranslateP = styled.p`
 font-family: 'Ranade', sans-serif;
 font-size: 0.6rem;
+font-weight: bolder;
+position: relative;
+top: 11vh;
+right: 8.5vw;
+color: #0263F5;
+&:hover{
+    cursor: pointer;
+
+}
+`
+const NdInfo = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+const NdTranslateP = styled.p`
+font-family: 'Ranade', sans-serif;
+font-size: 0.6rem;
+font-weight: bolder;
+position: relative;
+top: 11vh;
+right: 32vw;
+color: #0263F5;
+&:hover{
+    cursor: pointer;
+
+}
+`
+const NdBorderImgDiv = styled.div`
+border: solid 6px #0263F5;
+border-radius: 5px;
+width: 12vw;
+height: 42vh;
+position: relative;
+right: 10vw;
+`
+const NdImg = styled.img`
+width: 100%;
+height: 40.7vh;
+margin: 2vh;
+border: solid 4px #CCCCCD;
+border-radius: 5px;
+`
+const Hr = styled.hr`
+margin: 2vw;
+border: solid 2px #CCCCCD;
 `
 
 export default function About() {
@@ -53,12 +96,20 @@ export default function About() {
 
             <FirstInfo>
             <BorderImgDiv>
-                <Img src={img1} alt="Leonardo Vantine"></Img>
+                <Img src={img2} alt="Leonardo Vantine"></Img>
             </BorderImgDiv>
             <P>Meu nome é Leonardo Vantine e tenho 17 anos de idade. Atualmente moro em Santana do Deserto, Minas Gerais. Morei em Três Rios, Rio de Janeiro até os meus 13 anos de idade. Sempre fui muito determinado e disposto a mudanças e novidades. No ano de 2019 me mudei para Minas Gerais em busca de oportunidades de vida para meu crescimento pessoal e profissional. Me tornei jovem líder da Onda Solidária e resultou em muitas oportunidades.</P>
-            <LanguageButton>TRADUZIR PARA INGLÊS</LanguageButton>
+            <TranslateP>TRADUZIR PARA INGLÊS</TranslateP>
             </FirstInfo>
-
+            <Hr></Hr>
+            <NdInfo>
+                <P>Sempre gostei de tecnologia e sentia que algum dia iria atuar em alguma área TI, mas não tinha noção do quão grande é esse mundo. Atráves da Onda Solidária conheci o Vai na Web, logo quando se abriu as inscrições para o curso de Desenvolvedor Front-end. Resolvi me inscrever e fui percebendo com o tempo que é algo que realmente quero me aperfeiçoar cada vez mais e crescer profissionalmente.</P>
+                <NdTranslateP>TRADUZIR PARA INGLÊS</NdTranslateP>
+                <NdBorderImgDiv>
+                    <NdImg src={img1} alt="Leonardo Vantine"></NdImg>
+                </NdBorderImgDiv>
+            </NdInfo>
+            <Hr></Hr>
         </Container>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MainDiv = styled.div`
-border: solid red;
+/* border: solid red; */
 width: 100%;
 height: 87vh;
 background-color: #1B1D22;
